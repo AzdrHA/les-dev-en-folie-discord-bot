a387969c489e8ede690d7845c070662c24b9bcc0
Azdra <baptiste.brand2002@gmail.com>
2021-10-18 22:47:02 +0200

    Add default whiteLink

--- src/app.ts
+++ src/app.ts
@@ -10,7 +10,17 @@ const client = new Client({intents: [Intents.FLAGS.GUILDS,
 ]});
 
 const guildWhiteList = ['896803623886028900', '779076217877561402'];
-const defaultWhiteLink = ['https://www.youtube.com'];
+const defaultWhiteLink = ['https://www.youtube.com', 'https://developer.mozilla.org', 'https://stackoverflow.com',
+  'https://github.com', 'https://gitlab.com', 'https://www.gitkraken.com', 'https://www.docker.com', 'https://reactjs.org',
+  'https://fr.reactjs.org', 'https://vuejs.org', 'https://angular.io', 'https://nuxtjs.org', 'https://nextjs.org',
+  'https://nestjs.com', 'https://www.wikipedia.org', 'https://fr.wikipedia.org', 'https://www.flaticon.com',
+  'https://imgbin.com', 'https://www.figma.com', 'https://app.diagrams.net', 'https://draw.io', 'https://getbootstrap.com',
+  'https://get.foundation', 'https://tailwindcss.com', 'https://materializecss.com', 'https://getmdl.io', 'https://bulma.io',
+  'http://getskeleton.com', 'https://semantic-ui.com', 'https://purecss.io', 'https://getuikit.com', 'https://www.drupal.org',
+  'https://www.drupal.fr', 'https://symfony.com', 'https://getcomposer.org', 'https://sylius.com', 'https://www.php.net',
+  'https://www.google.com',
+];
+
 const reLink = /(([a-z]+:\/\/)?(([a-z0-9\-]+\.)+([a-z]{2}|aero|arpa|biz|com|coop|edu|gov|info|int|jobs|mil|museum|name|nato|net|org|pro|travel|local|internal))(:[0-9]{1,5})?(\/[a-z0-9_\-\.~]+)*(\/([a-z0-9_\-\.]*)(\?[a-z0-9+_\-\.%=&amp;]*)?)?(#[a-zA-Z0-9!$&'()*+.=-_~:@/?]*)?)(\s+|$)/gi;
 
 client.on('ready', () => {
@@ -44,19 +54,15 @@ client.on('messageCreate', async (message) => {
     }
   }
 
-  console.log('1');
   if (message.content.slice(0, prefix.length).toLowerCase() !== prefix.toLowerCase()) return;
-  console.log('2');
 
   const args: string[] = message.content.slice(prefix.length).trim().split(/ +/g);
   const command = args.shift().toLowerCase();
-  console.log('3');
 
   if (command === 'whitelink') {
     if (!args[0]) return message.reply({content: 'Vous devez insérer un nom de domaine!'});
     const filesLink: string[] = require('../whiteLink.json');
     filesLink.push(args[0]);
-    console.log('4');
 
     try {
       fs.writeFileSync('whiteLink.json', JSON.stringify(filesLink));

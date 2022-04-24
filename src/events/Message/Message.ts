b7d330134a123eab129b79d9b68699354a3011a8
Azdra <baptiste.brand2002@gmail.com>
2022-04-24 12:22:52 +0200

    Debugging

--- src/events/Message/Message.ts
+++ src/events/Message/Message.ts
@@ -16,14 +16,20 @@ export const messageCreate = async (message: Message, client: App, prefixes: str
   const prefix = prefixes.find((p) => message.content.toLowerCase().startsWith(p));
   const args: string[] = prefix ? message.content.slice(prefix.length).trim().split(/ +/g) : null;
   const command = args ? args.shift().toLowerCase() : null;
+  console.log('1');
+
 
   if ((command && command !== 'whitelink') && !message.member.permissions.has('MANAGE_MESSAGES')) {
+    console.log('1.1');
+
     if (!excludeChannelVerify.includes(message.channel.id) && message.content.match(regexLink)) {
+      console.log('1.2');
       let filesLink: string[] = require(WhiteLinkManagerClass.filePath);
       filesLink = filesLink.concat(defaultWhiteLink);
 
       for (const match of message.content.match(regexLink)) {
         if (!filesLink.includes(match)) {
+          console.log('1.3');
           await message.delete();
           return message.channel.send(`${message.author.toString()}, ${translation.LINK_FORBIDDEN}`);
         }
@@ -31,15 +37,24 @@ export const messageCreate = async (message: Message, client: App, prefixes: str
     }
   }
 
+  console.log('2');
   if (command === 'whitelink') {
+    console.log('3');
     if (message.author.id !== '311874717504110593' && !message.member.permissions.has('MANAGE_MESSAGES')) {
+      console.log('3.1');
       return message.reply({content: translation.BAD_PERMISSION_TO_ADD_LINK});
     }
 
+    console.log('4');
     if (!args[0]) return message.reply({content: translation.NEED_INSERT_TO_DOMAIN_NAME});
+    console.log('5');
+
     WhiteLinkManagerClass.addLink(args[0]).then(() => {
+      console.log('6');
       return message.reply({content: translation.LINK_ADDED});
     }).catch(() => {
+      console.log('7');
+
       return message.reply({content: translation.LINK_ALREADY_ADDED});
     });
   }

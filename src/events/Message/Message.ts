b9727eb707610b4ce8929e8566a05dfb7ba620ae
Azdra <baptiste.brand2002@gmail.com>
2022-04-24 12:26:57 +0200

    Remove console log

--- src/events/Message/Message.ts
+++ src/events/Message/Message.ts
@@ -10,30 +10,21 @@ import App from '../../components/App/App';
 import * as translation from '../../message.json';
 
 export const messageCreate = async (message: Message, client: App, prefixes: string[]): Promise<any> => {
-  console.log('0.1');
   if (message.author.bot || message.guild && (!discordWhiteList.includes(message.guild.id))) return;
-  console.log('0.2');
-
   if (client.development && (message.channel.id !== '933413191994728510' || (message.author.id !== '311874717504110593' && message.author.id !== '649352959364169739'))) return;
-  console.log('0.3');
 
   const prefix = prefixes.find((p) => message.content.toLowerCase().startsWith(p));
   const args: string[] = prefix ? message.content.slice(prefix.length).trim().split(/ +/g) : null;
   const command = args ? args.shift().toLowerCase() : null;
-  console.log('1');
 
 
   if ((command && command !== 'whitelink') && !message.member.permissions.has('MANAGE_MESSAGES')) {
-    console.log('1.1');
-
     if (!excludeChannelVerify.includes(message.channel.id) && message.content.match(regexLink)) {
-      console.log('1.2');
       let filesLink: string[] = require(WhiteLinkManagerClass.filePath);
       filesLink = filesLink.concat(defaultWhiteLink);
 
       for (const match of message.content.match(regexLink)) {
         if (!filesLink.includes(match)) {
-          console.log('1.3');
           await message.delete();
           return message.channel.send(`${message.author.toString()}, ${translation.LINK_FORBIDDEN}`);
         }
@@ -41,24 +32,15 @@ export const messageCreate = async (message: Message, client: App, prefixes: str
     }
   }
 
-  console.log('2');
   if (command === 'whitelink') {
-    console.log('3');
     if (message.author.id !== '311874717504110593' && !message.member.permissions.has('MANAGE_MESSAGES')) {
-      console.log('3.1');
       return message.reply({content: translation.BAD_PERMISSION_TO_ADD_LINK});
     }
 
-    console.log('4');
     if (!args[0]) return message.reply({content: translation.NEED_INSERT_TO_DOMAIN_NAME});
-    console.log('5');
-
     WhiteLinkManagerClass.addLink(args[0]).then(() => {
-      console.log('6');
       return message.reply({content: translation.LINK_ADDED});
     }).catch(() => {
-      console.log('7');
-
       return message.reply({content: translation.LINK_ALREADY_ADDED});
     });
   }
